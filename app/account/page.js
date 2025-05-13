dea3f6b9445a8faead0e188685d22d63bb7014ae
williamjaysmith <williamjaysmith@gmail.com>
2025-05-13 08:06:00 -0500

    style app: Working on Responsiveness

--- app/account/page.js
+++ app/account/page.js
@@ -1,9 +1,17 @@
-import Link from "next/link";
+import { auth } from "../_lib/auth";
 
-export default function Page() {
+export const metadata = {
+  title: "Guest area",
+};
+
+export default async function Page() {
+  const session = await auth();
+
+  const firstName = session.user.name.split(" ").at(0);
+
   return (
-    <div>
-      <h1>account Page</h1>
-    </div>
+    <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+      Welcome, {firstName}
+    </h2>
   );
 }
