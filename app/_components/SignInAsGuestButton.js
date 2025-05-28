2457bb7a7513947b6bef48166456b062ea7bab10
williamjaysmith <williamjaysmith@gmail.com>
2025-05-27 20:36:53 -0500

    adjusted login button widths / responsiveness

--- app/_components/SignInAsGuestButton.js
+++ app/_components/SignInAsGuestButton.js
@@ -5,7 +5,7 @@ import { signInAsGuest } from "../_lib/actions";
 function SignInAsGuestButton() {
   return (
     <form action={signInAsGuest}>
-      <button className="flex w-96 items-center justify-center gap-6 whitespace-nowrap bg-primary-900 py-3 text-lg font-medium">
+      <button className="flex w-72 items-center justify-center gap-6 whitespace-nowrap bg-primary-900 py-3 text-lg font-medium sm:w-96">
         <span className="text-2xl">👉</span>
         <span>Continue as Guest</span>
       </button>
--- app/_components/SignInButton.js
+++ app/_components/SignInButton.js
@@ -3,7 +3,7 @@ import { signInAction } from "../_lib/actions";
 function SignInButton() {
   return (
     <form action={signInAction}>
-      <button className="mb-4 flex w-96 items-center justify-center gap-6 whitespace-nowrap bg-primary-900 py-3 text-lg font-medium">
+      <button className="mb-4 flex w-72 items-center justify-center gap-6 whitespace-nowrap bg-primary-900 py-3 text-lg font-medium sm:w-96">
         <img
           src="https://authjs.dev/img/providers/google.svg"
           alt="Google logo"
